0319d33060a5c4f5194d91ff64abbc3e355386e7
Preetish Sharma <preet06072003@gmail.com>
2024-06-16 00:36:03 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
@@ -22,3 +23,4 @@ app.listen(3000, () => {
 app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
