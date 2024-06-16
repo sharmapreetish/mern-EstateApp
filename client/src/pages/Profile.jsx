1016fec2c93d4596ce18d501f8a33073b606ab44
Preetish Sharma <preet06072003@gmail.com>
2024-06-16 22:50:46 +0530

    completed profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+ 
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+
   return (
-    <div>
-      Profile
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          id='username'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>
+        <span className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+      </div>
+
     </div>
-  )
+  );
 }
